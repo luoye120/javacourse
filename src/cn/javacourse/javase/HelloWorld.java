b97975d1d299c78ef174d4cf33bf5da7812b19c1
yangshuai <715496545@qq.com>
2018-04-01 19:57:36 +0800

    helloword

---
+++ src/cn/javacourse/javase/HelloWorld.java
@@ -0,0 +1,19 @@
+package cn.javacourse.javase;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        bb();
+        System.out.println("hello world");
+    }
+
+    public static void bb(){
+        System.out.println("bbb");
+        System.out.println("bbb");
+        System.out.println("bbb");
+
+    }
+}
